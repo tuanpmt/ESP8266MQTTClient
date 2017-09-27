295dc65ae6351685d195b01044b5c85b748d0db2
Tuan PM <tuanpm@live.com>
2017-09-27 14:15:31 +0700

    enhance uri parser, resolve #5

--- src/uri_parser.h
+++ src/uri_parser.h
@@ -12,13 +12,15 @@ typedef struct {
 	char *fragment;             /* optional */
 	char *username;             /* optional */
 	char *password;             /* optional */
+    char *extension;
+    char *host_ext;
 	char *_uri;             	/* private */
 	int _uri_len;             	/* private */
 } parsed_uri_t;
 
 parsed_uri_t *parse_uri(const char *);
 void free_parsed_uri(parsed_uri_t *);
-
+void parse_uri_info(parsed_uri_t *puri);
 #ifdef __cplusplus
 }
 #endif
